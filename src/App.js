745dd1b1e96f0e7d8171494b165e0f6e66bc4e26
askat123 <asylbekuuluaskat11@gmail.com>
2023-07-22 15:37:25 +0600

    task1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import MySchool from './components/Myschool';
+import AboutUs from './components/Aboutus';
+import OurCours from './components/Ourcours';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Routes>
+        <Route path='/' element={ <Home/>}/>
+        <Route path='/myschool' element={ <MySchool/>}/>
+        <Route path='/ourcours' element={ <OurCours/>}/>
+        <Route path='/aboutus' element={ <AboutUs/>}/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./index.scss"
+import logo from "../../img/logo.png"
+import { BsInstagram } from 'react-icons/bs';
+import { AiOutlineYoutube } from 'react-icons/ai';
+import { PiFacebookLogo } from 'react-icons/pi';
+import { FiTwitter } from 'react-icons/fi';
+import { FaTiktok } from 'react-icons/fa';
+import { LiaTelegramPlane } from 'react-icons/lia';
+import { RxEnvelopeClosed } from 'react-icons/rx';
+import { BsTelephone } from 'react-icons/bs';
+import { FiMapPin } from 'react-icons/fi';
+
+const Footer = () => {
+  return (
+    <div id="footer">
+      <div className="container">
+        <div className="footer">
+          <div className="footer--logo">
+            <Link>
+              <img src={logo} alt="" />
+            </Link>
+            <div>
+              <button>KG</button>
+              <button>RU</button>
+            </div>
+          </div>
+          <div className="footer--about">
+            <h3>Быстрые ссылки</h3>
+            <Link>О школе</Link>
+            <Link>Наши курсы</Link>
+            <Link>О нас</Link>
+          </div>
+          <div className="footer--social">
+            <h3>Социальные сети</h3>
+            <div className="footer--social__icon">
+              <BsInstagram />
+              <AiOutlineYoutube />
+              <PiFacebookLogo />
+            </div>
+            <div className="footer--social__icon">
+              <FiTwitter />
+              <FaTiktok />
+              <LiaTelegramPlane />
+            </div>
+          </div>
+          <div className="footer--contacts">
+            <h3>Контакты</h3>
+            <a href="@motionwebteam@gmail.com">
+              <RxEnvelopeClosed />
+              @motionwebteam@gmail.com
+            </a>
+            <a href="" tel={+996559692626}>
+              <BsTelephone />
+              +996 559 - 69 - 26 - 26
+            </a>
+          </div>
+          <div className="footer--address">
+            <h3>Адреса</h3>
+            <p>
+              <FiMapPin />
+              Турусбекова 109 / 3
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
